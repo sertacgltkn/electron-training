f6d73267c2b87b0bd79e795fdf4224efa1003b4f
Sertaç Gültekin <sertacgltkn@gmail.com>
2023-01-31 20:31:05 +0300

    dev tools for tab

--- main.js
+++ main.js
@@ -2,9 +2,11 @@ const electron = require("electron");
 const url = require("url");
 const path = require("path");
 
-const { app, BrowserWindow } = electron;
+const { app, BrowserWindow, Menu } = electron;
 
 let mainWindow;
+
+console.log(process.platform);
 
 app.on("ready", () => {
   mainWindow = new BrowserWindow({});
@@ -17,5 +19,51 @@ app.on("ready", () => {
     })
   );
 
-
+  const mainMenu = Menu.buildFromTemplate(mainMenuTemplate);
+  Menu.setApplicationMenu(mainMenu);
 });
+
+const mainMenuTemplate = [
+  {
+    label: "Dosya",
+    submenu: [
+      {
+        label: "Yeni ToDo Ekle",
+      },
+      {
+        label: "Tümünü Sil",
+      },
+      {
+        label: "Cikis",
+        accelerator: process.platform == "darwin" ? "Command+Q" : "Ctrl+Q",
+        role: "quit",
+      },
+    ],
+  },
+];
+
+// menu for mac
+if (process.platform === "darwin") {
+  mainMenuTemplate.unshift({
+    label: app.getName(),
+    role: "TODO",
+  });
+}
+
+if (process.env.NODE_ENV !== "production") {
+  mainMenuTemplate.push({
+    label: "Dev Tools",
+    subMenu: [
+      {
+        label: "Geliştirici Penceresini Aç",
+        click(item, focusedWindow) {
+          focusedWindow.toggleDevTools();
+        },
+      },
+      {
+        label: "Yenile",
+        role: "reload",
+      },
+    ],
+  });
+}
